cc8368881af53d32750718fc55a2ca5ae21cf83f
Aitor Palomares <aitorpalomares@gmail.com>
2017-01-27 14:50:48 +0000

    HTML footer

--- app/html/footer.php
+++ app/html/footer.php
@@ -1,27 +1,12 @@
 <div id="footer">
 	<div class="container">
 		<div class="row">
-			<ul class="xs-12 sm-6 md-3">
-				<li>ABOUT US</li>
-				<li><a href="#" class="link">Link 1</a></li>
-				<li><a href="#" class="link">Link 2</a></li>
-				<li><a href="#" class="link">Link 3</a></li>
-				<li><a href="#" class="link">Link 4</a></li>
-			</ul>
-			<ul class="xs-12 sm-6 md-3">
-				<li>OTHER LINKS</li>
-				<li><a href="#" class="link">Link 1</a></li>
-				<li><a href="#" class="link">Link 2</a></li>
-			</ul>
-			<ul class="xs-12 sm-6 md-3">
-				<li>MORE LINKS</li>
-				<li><a href="#" class="link">Link 1</a></li>
-				<li><a href="#" class="link">Link 2</a></li>
-				<li><a href="#" class="link">Link 3</a></li>
-			</ul>
-			<ul class="xs-12 sm-6 md-3">
-				<li>LATEST NEWS</li>
-				<li><a href="#" class="link">Link 1</a></li>
+			<ul class="xs-12">
+				<li>© 1997-2015 Ancestry</li>
+				<li><a href="#" class="link">Corporate Information</a></li>
+				<li><a href="#" class="link">Privacy</a></li>
+				<li><a href="#" class="link">Terms and Conditions</a></li>
+				<li><a href="#" class="link">Site Map</a></li>
 			</ul>
 		</div>
 	</div>
