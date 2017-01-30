7342a0060d50efa715b4ce1414f15aae78e8f5be
torian12321 <aitorpalomares@hotmail.com>
2017-01-30 01:00:42 +0000

    starting with pannels

--- app/index.php
+++ app/index.php
@@ -1,9 +1,51 @@
 <?php require_once __dir__ . '/html/_header.php'; ?>
 
+
+<div id="header">
+	<div class="container">
+		<div class="row">			
+			
+			<ul class="breadcrumbs">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Search</a></li>
+				<li><a>Level 3</a></li>
+			</ul>
+
+		</div>
+	</div>
+</div>
+
+
+<div id="main-area">
+	
 <div class="container">
 	<div class="row">
-		Content
+
+		<div class="xs-12 sm-8 md-10">
+			<div class="panel">
+				<h2>World conflicts. Personal <stories class=""></stories></h2>
+				<button class="btn">Search all millitary records</button>
+				<button class="btn btn-main">Search all millitary records</button>
+				<button class="btn btn-sec">Search all millitary records</button>
+			</div>
+		</div>
+
+		<div class="xs-12 sm-4 md-2">
+			<div class="panel">
+				<h2>World conflicts. Personal <stories class=""></stories></h2>
+			</div>
+		</div>
+
+		<div class="xs-12 sm-4 md-2">
+			<div class="panel">
+				<h2>World conflicts. Personal <stories class=""></stories></h2>
+			</div>
+		</div>
+
+
+
 	</div>
+</div>
 </div>
 
 <?php require_once __dir__ . '/html/footer.php'; ?>
